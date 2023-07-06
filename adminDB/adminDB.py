a935cbcd8c34ed77a3d7f6420444b4df93eee291
AgustinLunardello <agustinlunardello030504@gmail.com>
2023-07-06 18:53:47 -0300

    add tasks linked to the user id

--- adminDB/adminDB.py
+++ adminDB/adminDB.py
@@ -13,24 +13,24 @@ class AdminDB:
             cursor = conn.cursor()
             
             query = '''CREATE TABLE IF NOT EXISTS Users (
-                id TEXT PRIMARY KEY,
+                id VARCHAR PRIMARY KEY,
                 name TEXT,
                 lastName TEXT,
                 bornDate DATE,
-                username TEXT,
-                password TEXT)'''   
+                username VARCHAR(25) UNIQUE,
+                password VARCHAR)'''   
             
             cursor.execute(query)
             conn.commit()
             
             query = '''CREATE TABLE IF NOT EXISTS Tasks (
-                id INTEGER PRIMARY KEY,
-                tittle TEXT,
-                description TEXT,
-                state TEXT,
-                created DATE,
-                updated DATE,
-                author TEXT)'''
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                tittle VARCHAR(50) NOT NULL,
+                description VARCHAR(50) NOT NULL,
+                state VARCHAR(25) NOT NULL,
+                created DATE NOT NULL,
+                updated DATE NOT NULL,
+                user_id VARCHAR)'''
             
             cursor.execute(query)
             conn.commit()
@@ -60,33 +60,45 @@ class AdminDB:
                 return True
             return False
     
-    def __get_id(self):
+    def __get_last_id(self):
         with sql.connect(self.path) as conn:
             cursor = conn.cursor()
             cursor.execute('''SELECT id FROM Tasks ORDER BY id DESC''')
             
             id = cursor.fetchone()
             if id :
-                return id[0] + 1
-            return 0 
+                return id[0]
+    
+    def __get_user_id(self,username: str):
+        with sql.connect(self.path) as conn:
+            cursor = conn.cursor()
+            
+            query = 'SELECT id FROM Users WHERE username = ?'
+            data = (username,)
+            
+            cursor.execute(query, data)
+            user_id = cursor.fetchone()
+            
+            if user_id:
+                return user_id[0]
+    
     
     def addTask(self, task: taskModel):
-        new_id = self.__get_id()
         with sql.connect(self.path) as conn:   
             cursor = conn.cursor()
-            query = '''INSERT INTO Tasks (id, tittle, description, state, created, updated, author) VALUES (?,?,?,?,?,?,?)'''
-            data = (new_id, task.tittle, task.description, task.state, str(datetime.now().strftime('%d-%m-%Y')), str(datetime.now().strftime('%d-%m-%Y')), task.author )
+            query = '''INSERT INTO Tasks (tittle, description, state, created, updated, user_id) VALUES (?,?,?,?,?,?)'''
+            data = (task.tittle, task.description, task.state, str(datetime.now().strftime('%d-%m-%Y')), str(datetime.now().strftime('%d-%m-%Y')), self.__get_user_id(task.author))
             
             cursor.execute(query, data)
             conn.commit()
-            return new_id
+            return self.__get_last_id()
 
     def get_all_tasks(self, author: str):
         with sql.connect(self.path) as conn:
             cursor = conn.cursor()
             
-            query = '''SELECT * FROM Tasks WHERE author = ? '''
-            data = (author,)
+            query = '''SELECT * FROM Tasks WHERE user_id = ? '''
+            data = (self.__get_user_id(author),)
             cursor.execute(query, data)
             
             tasksData = cursor.fetchall()
@@ -100,7 +112,6 @@ class AdminDB:
                         'state': taskData[3],
                         'created': taskData[4],
                         'updated': taskData[5],
-                        'author': taskData[6]
                     }
                     tasksList.append(task)    
                 return tasksList
@@ -110,8 +121,8 @@ class AdminDB:
         with sql.connect(self.path) as conn:
             cursor = conn.cursor()
             
-            query = '''SELECT * FROM Tasks WHERE id = ? AND author = ? '''
-            data = (id, author)
+            query = '''SELECT * FROM Tasks WHERE id = ? AND user_id = ? '''
+            data = (id, self.__get_user_id(author))
             
             cursor.execute(query, data)
             
@@ -124,7 +135,6 @@ class AdminDB:
                     'state': taskData[3],
                     'created': taskData[4],
                     'updated': taskData[5],
-                    'author': taskData[6]
                 }
                 return task
             return None
@@ -133,8 +143,8 @@ class AdminDB:
         with sql.connect(self.path) as conn:
             cursor = conn.cursor()
             
-            query = '''DELETE FROM Tasks WHERE id = ? AND author = ?'''
-            data = (id, author)
+            query = '''DELETE FROM Tasks WHERE id = ? AND user_id = ?'''
+            data = (id, self.__get_user_id(author))
             
             cursor.execute(query, data)
             conn.commit()
@@ -145,8 +155,8 @@ class AdminDB:
         with sql.connect(self.path) as conn:
             cursor = conn.cursor()
             
-            query = '''UPDATE Tasks SET state = ?, updated = ? WHERE id = ? AND author = ?'''
-            data = (new_state, datetime.now().strftime('%d-%m-%Y'), id, author)
+            query = '''UPDATE Tasks SET state = ?, updated = ? WHERE id = ? AND user_id = ?'''
+            data = (new_state, datetime.now().strftime('%d-%m-%Y'), id, self.__get_user_id(author))
             
             cursor.execute(query,data)
             conn.commit()
