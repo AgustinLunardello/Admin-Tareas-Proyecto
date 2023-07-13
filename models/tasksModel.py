66cd92b73ee8565b17db5abec4e0e757ef2ed3c0
AgustinLunardello <126476927+AgustinLunardello@users.noreply.github.com>
2023-07-12 23:28:44 -0300

    Delete models directory

--- models/tasksModel.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class taskModel(BaseModel):
-    tittle: str
-    description: Optional[str]
-    state: Optional[str]
-    author: str
