0d7b0ac6ff3e40ae414a4eba2ec35f7c7647d105
AgustinLunardello <126476927+AgustinLunardello@users.noreply.github.com>
2023-07-12 23:28:31 -0300

    Delete settings directory

--- settings/settings.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from hashlib import sha256
-
-class Settings(BaseModel):
-    authjwt_secret_key: str = sha256('token'.encode()).hexdigest()
-    authjwt_access_token_expires = 3600
